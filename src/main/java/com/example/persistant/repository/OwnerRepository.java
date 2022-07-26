d3369e2bf5133854fb0ecf87825bcccc30101042
Jovakinn <max05012004@gmail.com>
2022-07-26 18:05:29 +0300

    unnecessary delete

--- src/main/java/com/example/persistant/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistant.repository;
-
-import com.example.persistant.entity.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
